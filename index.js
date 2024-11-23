ee36c39246d40d929ef6fbba91c51540874ac10e
kunalvvs <kunalvvs@gmail.com>
2024-11-23 13:00:47 +0530

    port changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 // const port = 8080;
-const port = process.env.PORT || 3000;  //for vercel deployment
+const port = process.env.PORT || 8080;  //for vercel deployment
 
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
